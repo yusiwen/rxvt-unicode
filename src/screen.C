d96534a12c4500ecfd788517c20bb705276129b2
root <root>
2005-02-22 21:41:50 +0000

    *** empty log message ***

--- src/screen.C
+++ src/screen.C
@@ -1163,7 +1163,7 @@ rxvt_term::scr_backindex ()
 void
 rxvt_term::scr_forwardindex ()
 {
-  int             row;
+  int row;
 
   if (screen.cur.col < TermWin.ncol - 1)
     scr_gotorc (0, 1, R_RELATIVE | C_RELATIVE);
@@ -1392,19 +1392,16 @@ rxvt_term::scr_erase_screen (int mode)
     }
 }
 
+#if ENABLE_FRILLS
 void
 rxvt_term::scr_erase_savelines ()
 {
   want_refresh = 1;
   ZERO_SCROLLBACK ();
 
-  for (int i = 0; i < TermWin.saveLines; ++i)
-    if (screen.text [i])
-      {
-        screen.tlen[i] = 0;
-        scr_blank_line (screen.text [i], screen.rend [i], (unsigned int)TermWin.ncol, DEFAULT_RSTYLE);
-      }
+  TermWin.nscrolled = 0;
 }
+#endif
 
 /* ------------------------------------------------------------------------- */
 /*
@@ -2916,9 +2913,6 @@ int
 rxvt_term::selection_request_other (Atom target, int selnum)
 {
   Atom sel;
-#ifdef DEBUG_SELECT
-  char *debug_xa_names[] = { "PRIMARY", "SECONDARY", "CLIPBOARD" };
-#endif
 
   selection_type |= selnum;
 
