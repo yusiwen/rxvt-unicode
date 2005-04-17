c4c143e11d2e39a9f87a89be57d7facd16679e43
root <root>
2005-04-17 22:36:12 +0000

    *** empty log message ***

--- src/rxvt.h
+++ src/rxvt.h
@@ -24,6 +24,7 @@
 #if ENABLE_FRILLS
 # define ENABLE_XEMBED 1
 # define ENABLE_EWMH   1
+# define CURSOR_BLINK  1
 #endif
 
 /*
@@ -645,6 +646,10 @@ enum {
   Rs_borderLess,
   Rs_lineSpace,
   Rs_pty_fd,
+  Rs_cursorUnderline,
+#endif
+#if CURSOR_BLINK
+  Rs_cursorBlink,
 #endif
 #if ENABLE_XEMBED
   Rs_embed,
@@ -654,7 +659,6 @@ enum {
   Rs_answerbackstring,
   Rs_tripleclickwords,
   Rs_insecure,
-  Rs_cursorBlink,
   Rs_pointerBlank,
   Rs_pointerBlankDelay,
   Rs_imLocale,
@@ -1476,7 +1480,11 @@ struct rxvt_term : zero_initialized, rxvt_vars {
   void scr_refresh (unsigned char refresh_type);
   bool scr_refresh_rend (rend_t mask, rend_t value);
   void scr_erase_screen (int mode);
+#if ENABLE_FRILLS
   void scr_erase_savelines ();
+  void scr_backindex ();
+  void scr_forwardindex ();
+#endif
   void scr_touch (bool refresh);
   void scr_expose (int x, int y, int width, int height, bool refresh);
   rxvt_fontset *scr_find_fontset (rend_t r = DEFAULT_RSTYLE);
@@ -1493,8 +1501,6 @@ struct rxvt_term : zero_initialized, rxvt_vars {
   void scr_add_lines (const unicode_t *str, int nlines, int len);
   void scr_backspace ();
   void scr_tab (int count, bool ht = false);
-  void scr_backindex ();
-  void scr_forwardindex ();
   void scr_gotorc (int row, int col, int relative);
   void scr_index (enum page_dirn direction);
   void scr_erase_line (int mode);
