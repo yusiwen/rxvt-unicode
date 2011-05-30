9553290ab419911c849bcf16b0bd1d1877d4a795
sf-exg <sf-exg>
2011-05-30 21:24:40 +0000

    Embed libptytty.

---
+++ src/ptytty_wrapper.C
@@ -0,0 +1,12 @@
+#include <config.h>
+#include "rxvt.h"
+
+#define PTYTTY_NO_LIBCPP
+
+#define PTYTTY_FATAL rxvt_fatal
+#define PTYTTY_WARN  rxvt_warn
+
+#include "fdpass.C"
+#include "logging.C"
+#include "proxy.C"
+#include "ptytty.C"
